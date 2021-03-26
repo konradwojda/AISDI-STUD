130a7c8a7fdeeb7161f72cdee54c5968eae5c1a8
Jan Jakub Rusak <01158887@pw.edu.pl>
2021-03-27 00:10:14 +0100

    Shell_sort function updated and tests added

--- shell_sort.py
+++ shell_sort.py
@@ -1,7 +1,18 @@
 from insertion_sort import insertion_sort
 
 
-def shell_sort(_list, gaps):
+def make_gaps(_list):
+    gaps = []
+    list_len = len(_list)
+    while list_len != 1:
+        list_len //= 2
+        gaps.append(list_len)
+    return gaps
+
+
+def shell_sort(_list, gaps=None):
+    if not gaps:
+        gaps = make_gaps(_list)
     for gap in gaps:
         for i in range(gap):
             _list[i::gap] = insertion_sort(_list[i::gap])
