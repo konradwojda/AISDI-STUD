2273f7b0c500f74e5a6c4a737a25865ae58982ab
Konrad Wojda <konrad2001@outlook.com>
2021-03-27 17:31:48 +0100

    Added in_place test for insertion sort function.

--- test_insertion_sort.py
+++ test_insertion_sort.py
@@ -1,6 +1,12 @@
 from insertion_sort import insertion_sort
 
 
+def test_in_place():
+    list = [3, 1, 2]
+    insertion_sort(list)
+    assert list == [1, 2, 3]
+
+
 def test_instertion_sort_case1():
     list = [4, 3, 5, 2, 1]
     assert insertion_sort(list) == [1, 2, 3, 4, 5]
