6f722e718b64c983a9dd906d4e0ea32bad0040a6
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-03-29 12:27:20 +0200

    change test_sorting

--- test_sorting.py
+++ test_sorting.py
@@ -7,7 +7,7 @@ from import_numbers import generate_sorted_numbers
 from shell_sort import shell_sort
 
 FUNCTIONS = {quicksort, insertion_sort, shell_sort, inbuild}
-SIZES = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000}
+SIZES = {*range(1000, 10001, 1000)}
 TEXT = import_words("pan-tadeusz.txt", 10000)
 NUMBERS = generate_sorted_numbers(10000)
 
