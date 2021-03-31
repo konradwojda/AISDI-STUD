1e14f908faeb5c9ec3b91b52d69822bd32b6ba6e
Konrad Wojda <konrad2001@outlook.com>
2021-03-31 13:49:54 +0200

    improved benchmarks

--- benchmark.py
+++ benchmark.py
@@ -6,6 +6,11 @@ ins_lst = []
 qck_lst = []
 shell_lst = []
 words_numbers = [10, 100, 1000, 2000, 3000, 4000, 5000, 6000]
+functions = {
+    'insertion_sort(list)': ins_lst,
+    'quicksort(list)': qck_lst,
+    'shell_sort(list)': shell_lst
+}
 n_lst = []
 for i in range(len(words_numbers)):
     x = words_numbers[i]
@@ -18,33 +23,15 @@ from model_i import import_words
 n = {x}
 list = import_words('pan-tadeusz.txt', n)
 '''
-    temp_times = []
-    # ins sort
-    for _ in range(4):
-        time = timeit.timeit('insertion_sort(list)', setup=sorting_func, number=1)
-        temp_times.append(time)
-    temp_times.remove(max(temp_times))
-    temp_times.remove(min(temp_times))
-    time = (temp_times[0] + temp_times[1])/2
-    ins_lst.append(time)
-    temp_times = []
-    # qck sort
-    for _ in range(4):
-        time = timeit.timeit('quicksort(list)', setup=sorting_func, number=1)
-        temp_times.append(time)
-    temp_times.remove(max(temp_times))
-    temp_times.remove(min(temp_times))
-    time = (temp_times[0] + temp_times[1])/2
-    qck_lst.append(time)
-    temp_times = []
-    # shell sort
-    for _ in range(4):
-        time = timeit.timeit('shell_sort(list)', setup=sorting_func, number=1)
-        temp_times.append(time)
-    temp_times.remove(max(temp_times))
-    temp_times.remove(min(temp_times))
-    time = (temp_times[0] + temp_times[1])/2
-    shell_lst.append(time)
+    for key, value in functions.items():
+        temp_times = []
+        for _ in range(4):
+            time = timeit.timeit(key, setup=sorting_func, number=1)
+            temp_times.append(time)
+        temp_times.remove(max(temp_times))
+        temp_times.remove(min(temp_times))
+        time = (temp_times[0] + temp_times[1])/2
+        value.append(time)
 
 plt.plot(n_lst, ins_lst, label='insertion sort', markersize=3)
 plt.plot(n_lst, qck_lst, label='quick sort', markersize=3)
