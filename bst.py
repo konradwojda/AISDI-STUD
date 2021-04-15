38d01c898b1f68141c44fd21946fc6b65d961278
Jan Rusak <01158887@pw.edu.pl>
2021-04-15 12:35:45 +0200

    Added find succesor, predecessor functions and remove node function

--- bst.py
+++ bst.py
@@ -25,6 +25,38 @@ class BST:
                 node = node.right_child
         return node
 
+    def find_min_key(self, node=None):
+        node = node if node else self.root
+        while node.left_child is not None:
+            node = node.left_child
+        return node
+
+    def find_max_key(self, node=None):
+        node = node if node else self.root
+        while node.right_child is not None:
+            node = node.right_child
+        return node
+
+    def find_succesor(self, node=None):
+        node = node if node else self.root
+        if node.right_child is not None:
+            return self.find_min_key(node.right_child)
+        node_tmp = node.parent
+        while node_tmp is not None and node_tmp.left_child != node:
+            node = node_tmp
+            node_tmp = node_tmp.parent
+        return node_tmp
+
+    def find_predecessor(self, node=None):
+        node = node if node else self.root
+        if node.left_child is not None:
+            return self.find_max_key(node.left_child)
+        node_tmp = node.parent
+        while node_tmp is not None and node_tmp.right_child != node:
+            node = node_tmp
+            node_tmp = node_tmp.parent
+        return node_tmp
+
     def add(self, value):
         if self.root is None:
             self.root = Node(value=value)
@@ -42,17 +74,28 @@ class BST:
                     break
                 node = node.right_child
 
-    def find_min_key(self, node=None):
-        node = node if node else self.root
-        while node.left_child is not None:
-            node = node.left_child
-        return node
-
-    def find_max_key(self, node=None):
-        node = node if node else self.root
-        while node.right_child is not None:
-            node = node.right_child
-        return node
+    def remove_node(self, value):
+        node = self.find(value)
+        if node.left_child is None or node.right_child is None:
+            y = node
+        else:
+            y = self.find_succesor(node)
+        if y.left_child is not None:
+            x = y.left_child
+        else:
+            x = y.right_child
+        if x is not None:
+            x.parent = y.parent
+        if y.parent is None:
+            self.root = x
+        else:
+            if y == y.parent.left_child:
+                y.parent.left_child = x
+            else:
+                y.parent.right_child = x
+        if y != node:
+            node.value = y.value
+        return y
 
 
 if __name__ == "__main__":
@@ -66,3 +109,6 @@ if __name__ == "__main__":
     tree.add(98)
     print(tree.find_max_key().value)
     print(tree.find_min_key().value)
+    print(tree.find_succesor(tree.find(45)).value)
+    tree.remove_node(56)
+    print(tree.root.value)
