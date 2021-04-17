fc6bfe30441641bcb45d8ff41b4feda9aab4bb5d
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-04-17 19:14:46 +0200

    add remove in avl

--- avl.py
+++ avl.py
@@ -77,6 +77,54 @@ class AVL(BST):
         node.rebalance()
         pivot.rebalance()
 
+    def remove_node(self, value):
+        lowest_affected_node = super().remove_node(value)
+
+        if lowest_affected_node:
+            previous_balance = lowest_affected_node.balance
+            lowest_affected_node.rebalance()
+            if (previous_balance != 0 or
+                    lowest_affected_node.balance not in {-1, 1}):
+                self._check_removed_balance(lowest_affected_node)
+
+    def _check_removed_balance(self, node):
+        if abs(node.balance) > 1:
+            new_root = self._new_rotated_root(node)
+            new_root_prev_bf = new_root.balance
+
+            self._repair_balance(node)
+
+            assert node.parent == new_root
+
+            if new_root_prev_bf == 0 and new_root.balance in {-1, 1}:
+                return
+
+            node = new_root
+
+        if node.parent:
+            previous_balance = node.parent.balance
+
+            if node.parent.left_child == node:
+                node.parent.balance += 1
+            else:
+                node.parent.balance -= 1
+
+            if previous_balance != 0 or node.parent.balance not in {-1, 1}:
+                self._check_removed_balance(node.parent)
+
+    def _new_rotated_root(self, node):
+        if node.balance > 0:
+            if node.right_child.balance < 0:
+                return node.right.left_child
+            else:
+                return node.right_child
+
+        else:
+            if node.left_child.balance > 0:
+                return node.left_child.right_child
+            else:
+                return node.left_child
+
 
 if __name__ == "__main__":
     values = [
@@ -84,4 +132,9 @@ if __name__ == "__main__":
         7, 34, 24, 12, 6724, 2341, 23, 15, 34]
     tree = AVL(values=values)
     tree.add(5)
+    tree.remove_node(5)
+    tree.remove_node(56)
+    tree.remove_node(2341)
+    tree.remove_node(12)
+    tree.remove_node(7)
     print(tree)
