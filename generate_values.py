1f60490365691010f8ba10e3b2a79a263528cce6
Konrad Wojda <01158997@pw.edu.pl>
2021-04-19 19:12:18 +0200

    changed benchmark

--- generate_values.py
+++ generate_values.py
@@ -6,10 +6,4 @@ def generate_random_numbers(count):
 
 
 def generate_ascending_numbers(count):
-    result = []
-    for x in range(count):
-        if x == 0:
-            result.append(randint(-10000, 10000))
-        else:
-            result.append(randint(result[x-1], 10000))
-    return result
+    return [x for x in range(count)]
