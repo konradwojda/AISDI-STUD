1e69b9f55467e792a7e90342b6e87163492d3e43
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-04-28 21:43:12 +0200

    Add Measure time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "Heap.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
 
 int main()
 {
-	return 0;
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    //your code here
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(t2 - t1).count();
+
+    cout << duration;
+    return 0;
 }
